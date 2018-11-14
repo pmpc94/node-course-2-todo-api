bc3abf236b82cc145a9e1fd4d57057c6a7a52e1a
pmpc94 <pmig.pc@gmail.com>
2018-11-14 23:45:38 +0100

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
   _id: twoUserId,
   email: 'jen@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: twoUserId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: twoUserId
 }];
 
 const populateTodos = (done) => {
